f067c5f5c84d91c0909d7899d37b06dacedd8fdc
PHET45 <phnjk2000@gmail.com>
2025-07-25 13:50:03 +0700

    feat: calculate and log average student score in getAverageStudentScore function

--- ex-5.js
+++ ex-5.js
@@ -6,7 +6,8 @@ const students = [
 ];
 
 function getAverageStudentScore(students) {
-  // Start coding here
+  return students.reduce((sum,b) => (sum + b.score),0)/ students.length
 }
 
-getAverageStudentScore(students); // Output: 87.5
+getAverageStudentScore(students);
+console.log(getAverageStudentScore(students)) // Output: 87.5
