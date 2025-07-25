d550c9c97ecf612cae513fed85e6bf2f43dbba50
KrittidejDev <krittidej.dev@gmail.com>
2025-07-25 12:25:08 +0700

    assignment javascript built in array

--- ex-5.js
+++ ex-5.js
@@ -7,6 +7,10 @@ const students = [
 
 function getAverageStudentScore(students) {
   // Start coding here
+  let result =
+    students.reduce((total, current) => total + current.score, 0) /
+    students.length;
+  console.log(result);
 }
 
 getAverageStudentScore(students); // Output: 87.5
